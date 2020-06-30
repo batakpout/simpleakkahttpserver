e4f75dc549e4e9f9da33ee255fd7979ede62f17f
Aamir Wani <aamir.wani@BRILLIO.COM>
2020-06-30 21:25:11 +0530

    removed redundant values from json object

--- src/main/scala/sprayjson/SprayJson7.scala
+++ src/main/scala/sprayjson/SprayJson7.scala
@@ -9,13 +9,13 @@ object SprayJson7 extends App {
 
   case class DeviceCredentialAdministrativeDomain(objectType: Option[String], name: Option[String])
 
-  case class DeviceCredentialMediationServerGroup(objectType: Option[String], id: Option[String], name: Option[String])
+  //case class DeviceCredentialMediationServerGroup(objectType: Option[String], id: Option[String], name: Option[String])
 
   case class PreviousHop(objectType: Option[String], id: Option[String], name: Option[String])
 
-  case class DeviceDetails(dcParent: DeviceCredentialParent, dcAdminDomain: DeviceCredentialAdministrativeDomain,
-                           dcMediationServerGroup: DeviceCredentialMediationServerGroup,
-                           previousHop: PreviousHop, objectType: Option[String], name: Option[String], id: Option[String], primaryIp: Option[String], userId: Option[String], password: Option[String],
+  case class DeviceDetails(dcParent: Option[DeviceCredentialParent], dcAdminDomain: Option[DeviceCredentialAdministrativeDomain],
+                           dcMediationServerGroup: Option[DeviceCredentialParent],
+                           previousHop: Option[PreviousHop], objectType: Option[String], name: Option[String], id: Option[String], primaryIp: Option[String], userId: Option[String], password: Option[String],
                            vendorName: Option[String], productName: Option[String], productVersion: Option[String], profile: Option[String], office: Option[String], location: Option[String],
                            chatUserId: Option[String], chatPassword: Option[String], engineId: Option[String],
                            applicationMonitorsTemplateId: Option[String], networkElementTemplateId: Option[String], nodeActionTemplateId: Option[String],
@@ -66,7 +66,7 @@ object SprayJson7 extends App {
     }
   }
 
-  object DeviceCredentialMediationServerGroup {
+ /* object DeviceCredentialMediationServerGroup {
     implicit val deviceCredentialMediationServerGroupFormat = new RootJsonFormat[DeviceCredentialMediationServerGroup] {
       def write(deviceCredentialMediationServerGroup: DeviceCredentialMediationServerGroup): JsValue = {
         JsObject(
@@ -83,7 +83,7 @@ object SprayJson7 extends App {
         DeviceCredentialMediationServerGroup(fields("object-type").map(_.convertTo[String]), fields("id").map(_.convertTo[String]), fields("name").map(_.convertTo[String]))
       }
     }
-  }
+  }*/
 
   object PreviousHop {
     implicit val previousHopFormat = new RootJsonFormat[PreviousHop] {
@@ -167,7 +167,7 @@ object SprayJson7 extends App {
           if(value == JsNull) (key -> None) else (key -> Some(value))
         }.toMap
         DeviceDetails(fields("parent").map(_.convertTo[DeviceCredentialParent]), fields("administrativeDomain").map(_.convertTo[DeviceCredentialAdministrativeDomain]),
-          fields("mediation-server-group").map(_.convertTo[DeviceCredentialMediationServerGroup]), fields("previous-hop").map(_.convertTo[PreviousHop]), fields("object-type").map(_.convertTo[String]),
+          fields("mediation-server-group").map(_.convertTo[DeviceCredentialParent]), fields("previous-hop").map(_.convertTo[PreviousHop]), fields("object-type").map(_.convertTo[String]),
           fields("name").map(_.convertTo[String]), fields("id").map(_.convertTo[String]), fields("primary-ip").map(_.convertTo[String]),
           fields("userid").map(_.convertTo[String]), fields("password").map(_.convertTo[String]), fields("vendor-name").map(_.convertTo[String]), fields("product-name").map(_.convertTo[String]), fields("product-version").map(_.convertTo[String]),
           fields("profile").map(_.convertTo[String]), fields("office").map(_.convertTo[String]), fields("location").map(_.convertTo[String]), fields("chat-userid").map(_.convertTo[String]), fields("chat-password").map(_.convertTo[String]),
@@ -192,7 +192,8 @@ object SprayJson7 extends App {
   val json = """[{"name":"Do not delete","id":"a0cca2f5-185d-4d68-b929-918df30020dc","primary-ip":"10.10.10.10","primary-port":443,"protocol":"Tl1","session":"Telnet","userid":null,"SSHuserid":null,"password":null,"SSHpassword":null,"connection-state":"Unknown","vendor-name":null,"device-type":"Unknown","product-name":null,"product-version":null,"profile":null,"connection-type":"Direct","office":null,"location":null,"xid":null,"availabilitystate-required":false,"availability-state":"Available","secondary-ip":null,"secondary-port":null,"longitude":0,"latitude":0,"description":null,"provisioned-state":"Managed","chat-required":false,"chat-userid":null,"chat-password":null,"crlf-required":false,"craft":null,"subtended-node":null,"mediation-server-group":{"id":"f4557090-46a1-4dcf-acf5-e6ae0ae1cff0","name":"med","object-type":"mediation-server-group"},"last-inventory-synchronization-timestamp":null,"network-element-template-id":"070ebf2c-8a1d-4df0-8070-f9acb79a7786","application-monitors-template-id":"c9f3a00d-11f7-43dc-8a3c-2c871f538e21","engineid":"","auth-protocol":null,"privacy-protocol":null,"tenantName":null,"previous-hop":{"id":null,"name":null,"object-type":"network-element"},"alternate-name":"","bgp-as-identifier":null,"ospf-router-id":null,"node-action-template-id":"be100ea8-1e63-4441-aee1-a16af92753c4","parent":{"name":"test_discovery","id":"1f0158d8-c50c-4ec0-b1d1-7216721a2115","object-type":"subnetwork"},"administrativeDomain":{"name":"Default","object-type":"administrative-domain"},"object-type":"network-element"}]"""
   val x: List[DeviceDetails] = json.parseJson.convertTo[List[DeviceDetails]]
   println(x)
-  val res: List[DeviceDetails] = x.map { x =>
+  val res: List[DeviceDetails] = x.map
+  { x =>
     x.copy(sSHuserid = Some("root"), sSHpassword = Some("123paswword"))
   }
 
